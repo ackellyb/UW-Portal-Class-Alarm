0f52dd526a6e6fcc4865fa1f70c720677f9990e3
Aaron Kelly-Barker <aaron.kelly.barker@gmail.com>
2014-07-23 00:05:12 -0400

    Added buttons. Woo
    This commit is really only to show that it works. Buttons show up and
    stuff. Have to work on other things, I will add more meat to this
    tomorrow

--- widgets/mclarizi/mclarizi.js
+++ widgets/mclarizi/mclarizi.js
@@ -1,1 +1,12 @@
-function mclarizi(userid, htmlId) { $(htmlId).html("<h1>mclarizis Widget</h1>"); } 
+function mclarizi(userid, htmlId) {
+	var templates = {};
+
+	/*
+	 * Initialize the widget.
+	 */
+	portal.loadTemplates("widgets/mclarizi/templates.json",
+		function (t) {
+			templates = t;
+			$(htmlId).html(templates.baseHtml);
+		});
+}
